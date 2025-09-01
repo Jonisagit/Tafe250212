e6b1dd7f084b332baf835d6ec3404bd0c1b421a5
Jonathan Lee <Jonathan.LEE@student.tafesa.edu.au>
2025-09-01 17:06:15 +0930

    Commit changes including development of MainMenu; addition of blank xamls for MortgageCalculator, CurrencyCalculator and TripCalculator; addition of click events for MainMenu buttons to these blank xamls and the existing Maths Calculator (still called MainPage) and an exit event for the ExitButton_Click; as well as changing the App.xaml.cs to open the app on the MainMenu.

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			this.InitializeComponent();
+		}
+
+		private void MathsCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame?.Navigate(typeof(MainPage));
+		}
+
+		private void MortgageCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame?.Navigate(typeof(MortgageCalculator));
+		}
+
+		private void CurrencyCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame?.Navigate(typeof(CurrencyCalculator));
+		}
+
+		private void TripCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame?.Navigate(typeof(TripCalculator));
+		}
+
+		private void ExitButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Exit();
+		}
+
+	}
+}
